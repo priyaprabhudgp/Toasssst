fef56c657155136d9a87c484979d8362381bb122
arie <a.y.liang9@gmail.com>
2026-02-02 09:51:04 -0800

    Finished image bank for gacha and radix setup

--- student-game-app/src/main.jsx
+++ student-game-app/src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Theme>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Theme>
 );
